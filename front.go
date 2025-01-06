208ac0fc10fa360fdbd54fc9c7bec7f1d53b9c89
Shane Hanna <shane.hanna@gmail.com>
2025-01-06 16:57:15 +0000

    Limiter interface.
    We need to be able to use a distributed limiter.

--- front.go
+++ front.go
@@ -1,15 +1,24 @@
 package front
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/hashicorp/go-retryablehttp"
 	"golang.org/x/time/rate"
 )
 
+var LimitStarter = rate.NewLimiter(rate.Limit(float64(50)/60), 1)
+var LimitGrowth = rate.NewLimiter(rate.Limit(float64(100)/60), 1)
+var LimitScale = rate.NewLimiter(rate.Limit(float64(200)/60), 1)
+
+type Limiter interface {
+	Wait(context.Context) error
+}
+
 type transport struct {
 	bearer    string
-	limiter   *rate.Limiter
+	limiter   Limiter
 	transport http.RoundTripper
 }
 
@@ -26,7 +35,7 @@ func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
 //
 // It handles injection of the Front bearer token, API backoff requests and rate limits.
 // See https://dev.frontapp.com/docs/rate-limiting
-func WithFrontClient(token string, limit float64) ClientOption {
+func WithFrontClient(token string, limiter Limiter) ClientOption {
 	// The retryablehttp client is used to handle Retry-After headers.
 	client := retryablehttp.NewClient()
 	client.RetryMax = 1
@@ -34,7 +43,7 @@ func WithFrontClient(token string, limit float64) ClientOption {
 	// Injects the authorization bearer token header into every request.
 	client.HTTPClient.Transport = &transport{
 		bearer:    token,
-		limiter:   rate.NewLimiter(rate.Limit(limit), 1),
+		limiter:   limiter,
 		transport: client.HTTPClient.Transport,
 	}
 
